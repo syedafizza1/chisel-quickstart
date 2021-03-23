f9f602e369e24a1ea90858e8d67f1873dcbccd95
fizza <fizzajaffery72@gmail.com>
2021-03-23 08:14:34 -0400

    ex2 done

---
+++ src/test/scala/LAB1/ex2.scala
@@ -0,0 +1,11 @@
+package LAB1
+import org.scalatest._
+import chiseltest._
+import chisel3._
+class countingtest extends FreeSpec with ChiselScalatestTester {
+
+  "LAB1 ex-2" in {
+    test(new counting(5.U))
+    
+  }
+}
