5d1f4ca5174f87b6b5b956656299616b89e65564
fizza <fizzajaffery72@gmail.com>
2021-04-02 11:10:55 +0500

    commit

--- src/test/scala/LAB3/TASK2.scala
+++ src/test/scala/LAB3/TASK2.scala
@@ -5,12 +5,19 @@ import chiseltest._
 import chisel3._
 
 class ImmediateTests extends FreeSpec with ChiselScalatestTester {
-
-  "lab3 task-2" in {
-    test(new Immediate()) { c =>
-      c.io.instr.poke("b11111111".U)
+    "Lab 3 Task 2" in {
+    test(new task2()) { c =>
+      // c.io.instr.poke(103838099.U)
+      c.io.instr.poke("h01700013".U)
       c.io.pc.poke(1.U)
-      c.io.immd_se.expect(1.U)
+      c.io.immd_se.expect(23.U)
+    //   c.io.arg_x.poke(20.U)
+    //   c.io.arg_y.poke(20.U)
+    //   c.io.fnct3.poke("b000".U)
+    //   c.io.branch.poke(1.B)
+    //   c.io.br_taken.expect(1.B)
     }
   }
+}
+ 
 }
